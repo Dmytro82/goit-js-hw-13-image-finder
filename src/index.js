31a3cedae03437d5181c6d9a97a7ba75a9c1a270
Dmytro82 <dmutro8228@gmail.com>
2021-11-08 22:51:21 +0200

    dz

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+
+import ImageFinder from './js/image-finder.js';
+
+const imageFinder = new ImageFinder(12, '#gallery', '#load-more-button');
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+};
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  const query = e.currentTarget.elements.query.value;
+  if (!query.trim()) {
+    alert('Введите запрос');
+    return;
+  }
+  imageFinder.findImages(query);
+}
---
+++ src/js/image-finder.js
@@ -0,0 +1,67 @@
+import PixabayClient from './apiService.js';
+import templatePrint from '../templates/card-image.hbs';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+export default class ImageFinder {
+  constructor(resultsPerPage, resultSelector, loadMoreBtnSelector) {
+    this.pixabayClient = new PixabayClient(resultsPerPage);
+    this.refs = {
+      result: document.querySelector(resultSelector),
+      loadMoreBtn: document.querySelector(loadMoreBtnSelector),
+    };
+    this.refs.result.addEventListener('click', this.onCardClick.bind(this));
+    this.refs.loadMoreBtn.addEventListener('click', this.loadMore.bind(this));
+  }
+
+  async findImages(query) {
+    this.clear();
+    this.show(
+      await this.pixabayClient.findImages(query),
+      this.pixabayClient.canLoadMore(),
+    );
+  }
+
+  async loadMore() {
+    this.show(
+      await this.pixabayClient.getNextPage(),
+      this.pixabayClient.canLoadMore(),
+    );
+    setTimeout(this.scrollToEnd.bind(this), 1000);
+  }
+
+  scrollToEnd() {
+    document.body.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  }
+
+  show(data, canLoadMore) {
+    if (canLoadMore) {
+      this.refs.loadMoreBtn.classList.remove('is-hidden');
+    } else {
+      this.refs.loadMoreBtn.classList.add('is-hidden');
+    }
+
+    const html = templatePrint(data);
+    this.refs.result.insertAdjacentHTML('beforeend', html);
+  }
+  showModal(imgUrl) {
+    const changeModalImage = `<img src=${imgUrl} alt="icon"/>`;
+    const instance = basicLightbox.create(changeModalImage);
+    instance.show();
+  }
+
+  onCardClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+      return;
+    }
+    this.showModal(e.target.dataset.source);
+  }
+
+  clear() {
+    this.refs.result.innerHTML = '';
+  }
+}
